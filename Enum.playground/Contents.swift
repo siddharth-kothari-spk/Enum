10d442f4b17a10175d43cd052c73f4efcfc06b92
siddharth-kothari-spk <siddharth.kothari.spk@gmail.com>
2023-04-16 15:21:06 +0530

    Associated values

--- Enum.playground/Contents.swift
+++ Enum.playground/Contents.swift
@@ -41,3 +41,35 @@ for beverage in Beverage.allCases {
 }
 
 // --------------------------------------------------------------------------------------------
+
+// Associated values
+
+// it’s sometimes useful to be able to store values of other types alongside these case values. This additional information is called an associated value, and it varies each time you use that case as a value in your code.
+
+enum Barcode {
+    case upc(Int, Int, Int, Int)
+    case qrCode(String)
+}
+
+var productBarcode = Barcode.qrCode("ABCDEFGHIJKLMNOP")
+print(productBarcode) // prints "qrCode("ABCDEFGHIJKLMNOP")"
+
+productBarcode = .upc(8, 85909, 51226, 3)
+
+switch productBarcode {
+case .upc(let numberSystem, let manufacturer, let product, let check):
+    print("UPC: \(numberSystem), \(manufacturer), \(product), \(check).") // prints "UPC: 8, 85909, 51226, 3."
+case .qrCode(let productCode):
+    print("QR code: \(productCode).")
+}
+
+// OR
+
+switch productBarcode {
+case let .upc(numberSystem, manufacturer, product, check):
+    print("UPC: \(numberSystem), \(manufacturer), \(product), \(check).") // prints "UPC: 8, 85909, 51226, 3."
+case let .qrCode(productCode):
+    print("QR code: \(productCode).")
+}
+
+// --------------------------------------------------------------------------------------------
